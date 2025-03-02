81e773f9e7b4261164ac5810e879b712589460f4
yeefip <yeefipp@gmail.com>
2025-03-02 23:52:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,64 +4,59 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
-  if (value == 2 || value == 3) return true;
-  if (value % 2 == 0 || value % 3 == 0) return false;
-  for (uint64_t i = 5; i * i <= value; i += 2) {
-      if (value % i == 0) return false;
+    if (value < 2) return false;
+    if (value == 2 || value == 3) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
+
+    for (uint64_t i = 5; i * i <= value; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0) return false;
     }
-    return true;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t count = 0; 
-  uint64_t num = 1;   
-  while (count < n) {
-    num++;
-      if (checkPrime(num)) {
-          count++;
-      }
-  }
+      uint64_t count = 0;
+    uint64_t num = 1;
+
+    while (count < n) {
+        num++;
+        if (checkPrime(num)) {
+            count++;
+        }
+    }
     return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t num = value;
-    while (true) {
+  uint64_t nextPrime(uint64_t value) {
+    uint64_t num = value + 1;
+    while (!checkPrime(num)) {
         num++;
-        if (checkPrime(num)) {
-            break;
-        }
     }
-    return num; 
+    return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0; 
-  uint64_t i = 2;
-   while (i < hbound) { 
-      if (checkPrime(i)) { 
-          sum += i;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
         }
-        i++;
     }
     return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t count = 0;
+      uint64_t count = 0;
     uint64_t lastPrime = 0;
-    uint64_t i = lbound;
-    while (i < hbound) {
+
+    for (uint64_t i = lbound; i < hbound; i++) {
         if (checkPrime(i)) {
-            if (lastPrime > 0) {
-                if (i - lastPrime == 2) {
-                    count++;
-                }
+            if (lastPrime > 0 && i - lastPrime == 2) {
+                count++;
             }
             lastPrime = i;
         }
-        i++;
     }
-    return count
+    return count;
 }
