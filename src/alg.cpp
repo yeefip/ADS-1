0b427d6330131a3f0a7564ab54d0670159e6aca2
yeefip <yeefipp@gmail.com>
2025-03-02 23:47:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,58 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+    if (value < 2) return false;
+    if (value == 2 || value == 3) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
+
+    for (uint64_t i = 5; i * i <= value; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0) return false;
+    }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+      uint64_t count = 0;
+    uint64_t num = 1;
+
+    while (count < n) {
+        num++;
+        if (checkPrime(num)) {
+            count++;
+        }
+    }
+    return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t nextPrime(uint64_t value) {
+    uint64_t num = value + 1;
+    while (!checkPrime(num)) {
+        num++;
+    }
+    return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+      uint64_t count = 0;
+    uint64_t lastPrime = 0;
+    for (uint64_t i = lbound; i < hbound; i++) {
+        if (checkPrime(i)) {
+            if (lastPrime > 0 && i - lastPrime == 2) {
+                count++;
+            }
+            lastPrime = i;
+        }
+    }
+    return count;
 }
